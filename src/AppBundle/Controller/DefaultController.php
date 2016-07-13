6edcffe3954ff4404e1064c4372e98833bf3a2da
ep320 <ep320cam@gmail.com>
2016-07-13 20:55:14 +0100

    First evidence of a table of papers

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,7 +15,37 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..'),
+        ]);
+    }
+
+    /**
+     * Shows main list of papers
+     *
+     * @Route("/papers", name="papers")
+     */
+    public function showListAction(Request $request)
+    {
+        return $this->render('papers/index.html.twig', [
+            'papers' => [[
+                'manNo' => 12342,
+                'corrAuth' => 'Smith',
+                'paperType' => 'TR',
+                'subArea1' => 'BIOCHEM',
+                'subArea2' => 'BIOPHYS'
+            ], [
+                'manNo' => 17945,
+                'corrAuth' => 'Jones',
+                'paperType' => 'RA',
+                'subArea1' => 'NEURO',
+                'subArea2' => 'CELL'
+            ], [
+                'manNo' => 16111,
+                'corrAuth' => 'McDougal',
+                'paperType' => 'SR',
+                'subArea1' => 'PLANT',
+                'subArea2' => 'EPI'
+            ]]
         ]);
     }
 }
