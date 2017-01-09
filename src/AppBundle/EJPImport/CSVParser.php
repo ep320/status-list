11f054616c0c01f6dc9c62d75c592b7552f071f6
ep320 <ep320cam@gmail.com>
2017-01-09 13:28:11 +0000

    Updating CSV parser for accepted papers to handle file format that contains all manuscript data, not yet working: Notice: Undefined index: manuscriptNo,

--- src/AppBundle/EJPImport/CSVParser.php
+++ src/AppBundle/EJPImport/CSVParser.php
@@ -29,7 +29,7 @@ class CSVParser
         $reader->setStrict(false);
         $reader->setHeaderRowNumber(3, CsvReader::DUPLICATE_HEADERS_INCREMENT);
         $headers = $reader->getColumnHeaders();
-        if (count($headers) === 10) {
+        if (count($headers) === 13) {
             return $this->parseAcceptedPaperCSV($reader);
         }
 
@@ -96,39 +96,47 @@ class CSVParser
          */
         $em = $this->em;
 
-        if (!in_array('MS number', $reader->getColumnHeaders())) {
+        if (!in_array('MS No.', $reader->getColumnHeaders())) {
             throw new \Exception('Please upload a correctly formatted .csv file (column headers in 4th row');
 
         }
 
         foreach ($reader as $row) {
 
+            $matches = [];
+            preg_match('#(?<articleType>[A-Z]+)-\w+-(?<manuscriptNo>[0-9]{5})(R(?<revision>\d+))?(-(?<hadAppeal>\w))?#',
+                $row['MS No.'], $matches);
+
+            /**
+             * @var $subjectArea SubjectArea
+             */
+            $subjectArea = $em->getRepository(SubjectArea::class)
+                ->findOneBy(['description' => $row['Major Subject Area(s)']]);
+
+            if (array_key_exists(intval($matches['manuscriptNo']), $ejpPapers)) {
+                $ejpPapers[intval($matches['manuscriptNo'])]->subjectAreaId2 = $subjectArea->getId();
+                continue;
+            }
 
             $ejpPaper = new EjpPaper();
-            $ejpPaper->setManuscriptNo(intval($row['MS number']));
-            $ejpPaper->setArticleTypeCode($row['Type']);
-            $ejpPaper->setTitle($row['Title']);
-
-            if ($row['First subject area'] !== ' ') {
-                $subjectArea1 = $em->getRepository(SubjectArea::class)
-                    ->findOneBy(['description' => $row['First subject area']]);
-
-                $ejpPaper->setSubjectAreaId1($subjectArea1->getId());
-            }
-
-            if ($row['Second subject area'] !== ' ') {
-                $subjectArea = $em->getRepository(SubjectArea::class)
-                    ->findOneBy(['description' => $row['Second subject area']]);
-
-                $ejpPaper->setSubjectAreaId2($subjectArea->getId());
-            }
-
-            $ejpPaper->setCorrespondingAuthor($row['Corresponding author']);
+            $ejpPaper->setManuscriptNo(intval($matches['manuscriptNo']));
+            $ejpPaper->setArticleTypeCode($matches['articleType']);
+            $ejpPaper->setCorrespondingAuthor(html_entity_decode($row['Corresponding Author']));
+            $ejpPaper->setRevision(isset($matches['revision']) ? intval($matches['revision']) : 0);
+            $ejpPaper->setHadAppeal(isset($matches['hadAppeal']) && $matches['hadAppeal'] == 'A');
+            $ejpPaper->setSubjectAreaId1($subjectArea->getId());
+            $ejpPaper->setInsightDecision($row['Insight?']);
+            $ejpPaper->setInsightComment($row['Justification']);
             $ejpPaper->setAbstract($row['Abstract']);
             $ejpPaper->setImpactStatement($row['Impact statement']);
-            $ejpPaper->setInsightDecision($row['Insight?']);
             $ejpPaper->setDigestAnswersGiven($row['Digest answers?']);
             $ejpPaper->setAccepted(True);
+
+            $matches = [];
+            preg_match('#([0-9]{4}-[0-9]{2}-[0-9]{2})#',
+                $row['Decision date'], $matches);
+
+            $ejpPaper->setAcceptedDate(date_create_from_format('Y-m-d', $matches[0]));
             $ejpPapers[$ejpPaper->manuscriptNo] = $ejpPaper;
         }
         return $ejpPapers;
